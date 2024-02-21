826469fd272b9cb87008adc145d62c4ff2eae9fa
Luis <luises250@gmail.com>
2024-02-21 19:49:05 +0100

    First Commit

---
+++ main.js
@@ -0,0 +1,2 @@
+import "./src/blackjack/index.js"; // poner el index y el .js es opcional porque se busca por defecto
+import "./style.css";
---
+++ src/blackjack/usecases/crear-carta-html.js
@@ -0,0 +1,16 @@
+export const crearCartaHTML = (carta) => {
+  /**
+   * Crea una carta en el HTML
+   * @param {String} carta carta
+   * @return {HTMLImageElement} imagen de retorno
+   */
+
+  if (!carta) throw new Error("La carta es un argumento obligatorio");
+
+  // <img class="carta" src="assets/cartas/2C.png">
+  const imgCarta = document.createElement("img");
+  imgCarta.src = `assets/cartas/${carta}.png`; //3H, JD
+  imgCarta.classList.add("carta");
+
+  return imgCarta;
+};
